69dea8369d924a5cd60719867921053842497d44
MiaSabre <mia.liu2016@gmail.com>
2017-06-21 13:17:32 -0700

    New callSearchRotatedSortedArray

--- src/Main.java
+++ src/Main.java
@@ -6,6 +6,7 @@ public class Main {
     public static void main (String arg[]) {
        //callStrStringMia();
        callStrString();
+       callSearchRotatedSortedArray();
     }
 
     public static void callStrStringMia() {
@@ -21,5 +22,11 @@ public class Main {
         String target = "cde";
         //StrString s = new StrString();
         System.out.println(StrString.strStr(source, target));
+    }
+
+    public static void callSearchRotatedSortedArray() {
+        int[] A = {4, 5, 6, 7, 0, 1, 2, 3};
+        int target = 4;
+        System.out.println(SearchRotatedSortedArray.searchRotatedArray(A,target));
     }
 }
